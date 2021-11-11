67345d57ee2cf479c735689b0c3f8e65a72bc32a
ElenaStepanchuk <podolyak365@gmail.com>
2021-11-11 18:22:04 +0200

    Task 2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -10,17 +10,22 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-refs.start.addEventListener('click', handleChangeColorBody);
-refs.stop.addEventListener('click', handleClearColorBody);
+refs.stop.setAttribute(`disabled`, true);
 
-function handleChangeColorBody() {
-  refs.body.style.backgroundColor = getRandomHexColor();
-  timeoutId = setInterval(() => {
-  refs.body.style.backgroundColor = getRandomHexColor();
-}, 1000);
+refs.start.addEventListener('click', handleBtnChangeColorBody);
+refs.stop.addEventListener('click', handleBtnClearColorBody);
+
+function handleBtnChangeColorBody() {
+  refs.stop.removeAttribute(`disabled`);
+    refs.body.style.backgroundColor = getRandomHexColor();
+    timeoutId = setInterval(() => {
+      refs.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  refs.start.setAttribute(`disabled`, true);
 }
-
-function handleClearColorBody() {
- clearInterval(timeoutId);  
-}
-
+ 
+function handleBtnClearColorBody() {
+  clearInterval(timeoutId);
+  refs.stop.setAttribute(`disabled`, true);
+  refs.start.removeAttribute(`disabled`);
+ }
---
+++ src/js/02-timer.js
@@ -0,0 +1,8 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const refs = {
+    input: document.querySelector('[input#datetime-picker]'),
+}
+console.log(refs.input);
