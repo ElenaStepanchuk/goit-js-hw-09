fd3f7a3e99e74d5ab1f7c26bdf7f4b04df9df1a8
ElenaStepanchuk <podolyak365@gmail.com>
2021-11-11 08:46:54 +0200

    task 1 end

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,21 +1,26 @@
 const refs = {
-    start: document.querySelector('button[data-start]'),
-    stop: document.querySelector('button[data-stop]'),
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
 }
 
-console.log(refs.start);
-console.log(refs.stop);
+let timeoutId = null;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-
-const color = getRandomHexColor();
-console.log(color);
-
 refs.start.addEventListener('click', handleChangeColorBody);
+refs.stop.addEventListener('click', handleClearColorBody);
 
 function handleChangeColorBody() {
-    body.style.backgroundColor = getRandomHexColor();
+  refs.body.style.backgroundColor = getRandomHexColor();
+  timeoutId = setInterval(() => {
+  refs.body.style.backgroundColor = getRandomHexColor();
+}, 1000);
 }
+
+function handleClearColorBody() {
+ clearInterval(timeoutId);  
+}
+
