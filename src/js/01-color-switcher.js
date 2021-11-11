a803a4898e82a631b2a1cc4180d7c5166b6eb21b
ElenaStepanchuk <podolyak365@gmail.com>
2021-11-11 07:44:33 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const refs = {
+    start: document.querySelector('button[data-start]'),
+    stop: document.querySelector('button[data-stop]'),
+}
 
+console.log(refs.start);
+console.log(refs.stop);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+
+const color = getRandomHexColor();
+console.log(color);
+
+refs.start.addEventListener('click', handleChangeColorBody);
+
+function handleChangeColorBody() {
+    body.style.backgroundColor = getRandomHexColor();
+}
